e0fe7cebb4311cd23ed5d7172b66ed15125fe511
laurenloco <laurenloco06@gmail.com>
2022-03-02 15:11:19 -0300

    Modify window size

--- include/sled_settings.h
+++ include/sled_settings.h
@@ -2,8 +2,8 @@
 #define sled_settings_h
 
 #define SLED_VERSION "0.2.0-dev"
-#define SLED_WINDOW_WIDTH 800
-#define SLED_WINDOW_HEIGHT 450
+#define SLED_WINDOW_WIDTH 930
+#define SLED_WINDOW_HEIGHT 604
 #define SLED_TITLE "SLED - Stupid Level EDitor"
 
 #endif // sled_settings_h
